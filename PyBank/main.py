c8a194535bf9d072c5965e2ac917c988f0c17404
caitbeatty <75456142+caitbeatty@users.noreply.github.com>
2021-01-19 21:19:04 -0500

    homework submit

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,7 +7,15 @@ import csv
 csvpath = os.path.join('.', 'Resources', '03-Python_Homework_Instructions_PyBank_Resources_budget_data.csv')
 
 
-# Read using CSV module
+
+# make empty lists 
+total_months = []
+total_amount= []
+profit_change = []
+
+
+
+# open using CSV module
 
 with open(csvpath) as csvfile:
 
@@ -15,9 +23,69 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 
 
-    # Read the header row first (skip this step if there is not a header)
+    # To add in the header row first 
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
-
-    # Read each row of data after the header
+    #print(f"CSV Header: {csv_header}")
+    # Read each row of data after the header 
     #for row in csvreader:
+        #print(row)
+    
+    #run through the rows in the file
+    for row in csvreader:
+
+        #loop through  months
+        total_months.append(row[0])
+        
+        #loop through profit
+        total_amount.append(int(row[1]))
+
+    
+#calculate total months   
+months= len(total_months)
+
+
+# loop through all amounts 
+num_loops= len(total_amount)-1
+for x in range(num_loops):
+
+ #append    
+    profit_change.append(total_amount[x+1]-total_amount[x])
+	        
+# Get  max profit change
+max_profit_change = max(profit_change)
+#Get min profit change 
+min_profit_change = min(profit_change)
+	
+
+# Figure out which month the max increase is connected to
+max_profit_month = profit_change.index(max(profit_change)) + 1
+# Figure out which month the max decrease is connected to
+min_profit_month = profit_change.index(min(profit_change)) + 1 
+	
+
+# print outputs
+	
+
+print("Financial Analysis")
+print("-------------")
+print(f"Total Months: {months}")
+print(f"Total: ${sum(total_amount)}")
+print(f"Average Change: {round(sum(profit_change)/len(profit_change),2)}")
+print(f"Greatest Increase in Profits: {total_months[max_profit_month]} (${(max_profit_change)})")
+print(f"Greatest Decrease in Profits: {total_months[min_profit_month]} (${(min_profit_change)})")
+	 
+
+#export to text file
+txtpath = os.path.join('.', 'txtfile_results.txt')
+with open(txtpath, "w") as f:   
+    
+    # Opens file and casts as f 
+    f.write("Financial Analysis\n")
+    f.write("-------------\n")
+    f.write(f"Total Months: {months}\n")
+    f.write(f"Total: ${sum(total_amount)}\n")
+    f.write(f"Average Change: {round(sum(profit_change)/len(profit_change),2)}\n")
+    f.write(f"Greatest Increase in Profits: {total_months[max_profit_month]} (${(max_profit_change)})\n")
+    f.write(f"Greatest Decrease in Profits: {total_months[min_profit_month]} (${(min_profit_change)})\n") 
+    
+
--- PyPol/main.py
+++ PyPol/main.py
@@ -1,5 +1,4 @@
-# First we'll import the os module
-# This will allow us to create file paths across operating systems
+# import the os module
 import os
 
 # Module for reading CSV files
@@ -7,14 +6,12 @@ import csv
 
 csvpath = os.path.join('.', 'Resources', '03-Python_Homework_Instructions_PyPoll_Resources_election_data.csv')
 
-# # Method 1: Plain Reading of CSV files
-# with open(csvpath, 'r') as file_handler:
-#     lines = file_handler.read()
-#     print(lines)
-#     print(type(lines))
+#create list of candidate names
+candidate = []
+candidate_vote = []
+candidate_index = 0
 
-
-# Method 2: Improved Reading using CSV module
+# open csv 
 
 with open(csvpath) as csvfile:
 
@@ -26,5 +23,61 @@ with open(csvpath) as csvfile:
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
 
-    # Read each row of data after the header
-    for row in csvreader:
+    # loop through to pull unique names and add to count
+    for row  in csvreader: 
+        if row [2] not in candidate: 
+            candidate.append(row[2]) 
+            candidate_vote.append(int(1))
+        else:
+            candidate_index = candidate.index(row[2])
+            candidate_vote[candidate_index] = candidate_vote[candidate_index] + 1
+
+   
+    #get total votes
+    total_votes = sum(candidate_vote)
+
+    #percentage vote
+    #percent = int(candidate_vote)/int(total_vote)
+
+    # determine winner
+    winner = candidate_vote.index(max(candidate_vote))
+
+#print statements
+print("election results")
+print("-------------")
+print (f"total votes {total_votes}")
+print("-------------")
+#print (candidate)
+#print (candidate_vote)
+
+for index in range(len(candidate)):
+    name = candidate[index]
+    percent = "{:.3f}".format (candidate_vote[index]/total_votes * 100)
+    print(f"{name}: {percent} % ({candidate_vote[index]})")
+
+print("-------------")
+print(f"winner:{candidate[winner]}")
+print("-------------")
+
+#export to text file
+
+#export to text file
+txtpath = os.path.join('.', 'txtfile_results.txt')
+with open(txtpath, "w") as f:   
+    
+    # Opens file and casts as f 
+    f.write("election results\n")
+    f.write("-------------\n")
+    f.write(f"total votes {total_votes}\n")
+    f.write("-------------\n")
+
+    for index in range(len(candidate)):
+        name = candidate[index]
+        percent = "{:.3f}".format (candidate_vote[index]/total_votes * 100)
+        f.write(f"{name}: {percent} % ({candidate_vote[index]})\n")
+
+    
+    f.write("-------------\n")
+    f.write(f"winner:{candidate[winner]}\n")
+    f.write("-------------\n")
+    
